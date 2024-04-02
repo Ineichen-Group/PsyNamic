44e0b0f8eadc44a6f41cae8321052cec3805c9c0
veralara.bernhard <veralara.bernhard@uzh.ch>
2024-04-02 14:57:46 +0200

    Update recipe

--- prodigy/recipe.py
+++ prodigy/recipe.py
@@ -1,33 +1,50 @@
 # reference code https://github.com/explosion/prodigy-recipes/tree/master/tutorials/span-and-textcat
 import spacy
-import prodigy 
+import prodigy
+import json
 from prodigy.components.preprocess import add_tokens
 from prodigy.components.loaders import JSONL
+
+# start Prodigy server
+# python -m prodigy span-and-textcat pubmed_psych en ./input/psychdelic_study_50_20240312.jsonl -F ./recipe.py
 
 
 @prodigy.recipe(
     "span-and-textcat",
     dataset=("Dataset to save annotations into", "positional", None, str),
     lang=("Language to use", "positional", None, str),
-    file_in=("Path to examples.jsonl file", "positional", None, str)
+    file_in=("Path to examples.jsonl file", "positional", None, str),
 )
-def custom_recipe(dataset, lang, file_in):
-    span_labels = ["Study type", "Substance", "Application", "Number of participants", "Sex of participants", "Clinical trial phase", "Adverse event(s)", "Conclusion"]
-    textcat_labels = ["clinical study", "not clinical study"]
 
+def custom_recipe(dataset, lang, file_in):
+    span_labels = ["Application"]
+    with open('labels.json', 'r') as infile:
+        labels =  json.load(infile)
+    
+    # concat all value lists
+    textcat_labels =  [label for sublist in labels.values() for label in sublist]
+    
     def add_options(stream):
         for ex in stream:
             ex['options'] = [
-                {"id": lab, "text": lab} for lab in textcat_labels
+                {"id": i, "text": lab} for i, lab in enumerate(textcat_labels)
             ]
             yield ex
-
+                  
+    def get_keymap(labels: dict[list]) -> dict[str, str]:
+        keymap = {}
+        i = 0
+        for key, values in labels.items():
+            for value in values:
+                keymap[str(i)] = key
+                i += 1
+        return keymap
+    
     nlp = spacy.blank(lang)
     stream = JSONL(file_in)
-    # stream = add_tokens(nlp, stream, use_chars=None)
     stream = add_tokens(nlp, stream)
-
     stream = add_options(stream)
+
     blocks = [
         {"view_id": "spans_manual"},
         {"view_id": "choice", "text": None},
@@ -40,25 +57,7 @@ def custom_recipe(dataset, lang, file_in):
             "lang": nlp.lang,
             "labels": span_labels,
             "blocks": blocks,
-            "keymap_by_label": {
-                "0": "species", 
-                "1": "species", 
-                "2": "species", 
-                "3": "species", 
-                "4": "species", 
-                "5": "species", 
-                "6": "species", 
-                "7": "species",
-                "8": "species",
-                "9": "outcome", 
-                "10": "outcome", 
-                "11": "outcome", 
-                "12": "outcome", 
-                "13": "outcome", 
-                "14": "control" 
-                
-                
-            },
+            "keymap_by_label": get_keymap(labels),
             "choice_style": "multiple"
         },
     }
