edc5a6bf3e738bceb297ae32e38806e866e6d647
veralara.bernhard <veralara.bernhard@uzh.ch>
2024-04-16 16:23:30 +0200

    Add pattern highlights and proper export

--- prodigy/recipe.py
+++ prodigy/recipe.py
@@ -3,9 +3,14 @@ import spacy
 import prodigy
 import json
 import seaborn as sns
+from typing import Optional
+from prodigy import recipe, log, get_stream
 from prodigy.components.preprocess import add_tokens
+from prodigy.models.matcher import PatternMatcher
 from prodigy.components.loaders import JSONL
 from prodigy.types import RecipeSettingsType
+import copy
+
 
 # start Prodigy server
 # python -m prodigy span-and-textcat pubmed_psych en ./input/psychdelic_study_50_20240312.jsonl -F ./recipe.py
@@ -16,48 +21,34 @@ from prodigy.types import RecipeSettingsType
     dataset=("Dataset to save annotations into", "positional", None, str),
     lang=("Language to use", "positional", None, str),
     file_in=("Path to examples.jsonl file", "positional", None, str),
+    patterns=("Path to match patterns file", "option", "pt", str),
+
 )
+def custom_recipe(
+    dataset: str,
+    lang: str,
+    file_in: str,
+    patterns: Optional[str] = None
+) -> RecipeSettingsType:
 
-def custom_recipe(dataset:str, lang: str, file_in: str) -> RecipeSettingsType:
+    # Fetch labels from json files
     with open('span_labels.json', 'r') as infile:
-        span_labels =  json.load(infile)
-        
-    with open('choice_labels.json', 'r') as infile:
-        labels =  json.load(infile)
-    
-    # Flatten the labels
-    textcat_labels =  [f'{group}: {label}' for group, sublist in labels.items() for label in sublist]
-    
-    # Get color palette for each label group
-    def convert_color(value: float):
-        """Convert a value between 0 and 1 to a color value between 0 and 255."""
-        return round(value * 255)
-    
-    color_palette = sns.color_palette("Paired", len(labels))
-    color_strings = [f'rgba({convert_color(r)}, {convert_color(g)}, {convert_color(b)}, 0.3)' for r, g, b in color_palette]
-    colors = [color for sublist, color in zip(labels.values(), color_strings) for label in sublist]
+        span_labels = json.load(infile)
 
-    def add_options(stream):
-        for ex in stream:
-            ex['options'] = [
-                {"id": i, "text": lab, "style": {"background-color": c}} for i, (lab, c) in enumerate(zip(textcat_labels, colors))
-            ]
-            yield ex
-      
-    # Hacky solution to use keymapping for grouping the labels        
-    def get_keymap(labels: dict[list]) -> dict[str, str]:
-        keymap = {}
-        i = 0
-        for key, values in labels.items():
-            for value in values:
-                keymap[str(i)] = key
-                i += 1
-        return keymap
-    
-    nlp = spacy.blank(lang)
-    stream = JSONL(file_in)
+    with open('choice_labels.json', 'r') as infile:
+        labels = json.load(infile)
+
+    colors = get_colors(labels)
+    keymap = get_keymap(labels)
+    # Flatten the labels
+    flat_labels = [f'{group}: {label}' for group,
+                   sublist in labels.items() for label in sublist]
+
+    nlp = spacy.load(f'blank:{lang}')
+    stream = get_stream(file_in)
+    stream = add_options_and_highlights(stream, flat_labels, colors, nlp, patterns)
     stream = add_tokens(nlp, stream)
-    stream = add_options(stream)
+    
     blocks = [
         {"view_id": "html", "html_template": '<p>doi: <a href="{{pubmed_url}}" target="_blank">{{doi}}</a></p><p>Published in: {{secondary_title}}</p>'},
         {"view_id": "spans_manual"},
@@ -71,7 +62,56 @@ def custom_recipe(dataset:str, lang: str, file_in: str) -> RecipeSettingsType:
             "lang": nlp.lang,
             "labels": span_labels,
             "blocks": blocks,
-            "keymap_by_label": get_keymap(labels),
+            "keymap_by_label": keymap,
             "choice_style": "multiple"
         },
     }
+
+def add_options_and_highlights(
+        stream,
+        flat_labels: list[str],
+        colors: list[str],
+        nlp: spacy.language.Language,
+        patterns: str):
+    
+    matcher = PatternMatcher(
+        nlp,
+        label_span=False, # no label added to the span
+        label_task=False, # no label on top level task
+        combine_matches=True, # show all matches in one task
+        all_examples=True, # alle examples are returned
+    )
+
+    matcher = matcher.from_disk(patterns)
+
+    options = [
+        {"id": i, "text": lab, "style": {"background-color": c}} for i, (lab, c) in enumerate(zip(flat_labels, colors))
+    ]
+    for score, eg in matcher(stream):
+        task = copy.deepcopy(eg)
+        task["options"] = options
+        yield task
+
+def convert_color(value: float):
+    """Convert a value between 0 and 1 to a color value between 0 and 255."""
+    return round(value * 255)
+
+def get_colors(labels: dict[str, list[str]]):
+    """Get a list of colors as a rbga strings, one for each label.
+    Can be fed directly into css"""
+    color_palette = sns.color_palette("Paired", len(labels))
+    color_strings = [f'rgba({convert_color(r)}, {convert_color(g)}, {convert_color(b)}, 0.3)' for r, g, b in color_palette]
+    colors = [color for sublist, color in zip(
+        labels.values(), color_strings) for label in sublist]
+    return colors
+
+# Hacky solution to use keymapping for grouping the labels
+def get_keymap(labels: dict[list]) -> dict[str, str]:
+    """ Use keymap to group the labels."""
+    keymap = {}
+    i = 0
+    for key, values in labels.items():
+        for value in values:
+            keymap[str(i)] = key
+            i += 1
+    return keymap
