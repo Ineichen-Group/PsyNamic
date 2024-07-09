6f80f1e6b2858b0ffa4f0f21e9d1fbe7d5597b45
veralara.bernhard <veralara.bernhard@uzh.ch>
2024-07-09 11:01:01 +0200

    Add newest datafile to analysis

--- data/psynamic_dataanalysis.py
+++ data/psynamic_dataanalysis.py
@@ -3,7 +3,8 @@ from prodigy_data_reader import ProdigyDataCollector
 
 def descriptive_analysis(list_jsonl: str):
     prodigy_data = ProdigyDataCollector(list_jsonl)
-    print(f'Total number: {len(prodigy_data)}, rejected: {prodigy_data.nr_rejected}')
+    print(
+        f'Total number: {len(prodigy_data)}, rejected: {prodigy_data.nr_rejected}')
     prodigy_data.visualize_dist()
     prodigy_data.visualize_dist('Study Type')
     prodigy_data.visualize_nr_dist()
@@ -14,10 +15,12 @@ def descriptive_analysis(list_jsonl: str):
 def main():
     list_jsonl = [
         'data/prodigy_exports/prodigy_export_ben_95_20240423_113434.jsonl',
-        'data/iaa/iaa_round1_50/iaa_resolution/prodigy_export_review_all_text_50_20240418_review_20240607_145354.jsonl',
-        'data/prodigy_exports/prodigy_export_ben_24_20240425_152801.jsonl'
+        'data/iaa/iaa_round1_50/iaa_resolution/prodigy_export_review_all_text_50_20240418_20240607_145354.jsonl',
+        'data/prodigy_exports/prodigy_export_ben_24_20240425_152801.jsonl',
+        'data/iaa/iaa_round2_40/iaa_resolution/prodigy_export_review_all_text_40_20240523_20240705_183405.jsonl',
     ]
     descriptive_analysis(list_jsonl)
+   
 
 
 if __name__ == '__main__':
