ffd04eb581c30540e1e1d1720a5f6147997a081c
veralara.bernhard <veralara.bernhard@uzh.ch>
2024-09-09 18:03:31 +0200

    Init Django

---
+++ app/psynamic/management/commands/import_predictions.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import json
+from django.core.management.base import BaseCommand, CommandParser
+from psynamic.models import Study, Prediction, Label, LabelClass, ClassGroup
+from ast import literal_eval 
+import os
+
+# python manage.py import_predictions /home/vera/Documents/Arbeit/CRS/PsychNER/model/experiments/pubmedbert_substances_20240902/predictions.csv /home/vera/Documents/Arbeit/CRS/PsychNER/model/experiments/pubmedbert_substances_20240902/params.json
+class Command(BaseCommand):
+    help = 'Import predictions from a CSV file into the database'
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument('pred_csv', type=str)
+        parser.add_argument('params_file', type=str)
+
+    def handle(self, *args, **kwargs):
+        pred_df = pd.read_csv(kwargs['pred_csv'])
+        with open(kwargs['params_file'], 'r') as file:
+            params = json.load(file)
+            
+        task = params['task']
+        data_path = params['data']
+        model = params['model']
+        
+        # get meta json in data path
+        # TODO: don't hardcode path
+        meta_file = os.path.join('/home/vera/Documents/Arbeit/CRS/PsychNER/', data_path, f'meta.json')
+        with open(meta_file, 'r') as file:
+            meta = json.load(file)
+            int_to_label = meta['Int_to_label']
+            int_to_label = {int(k): v for k, v in int_to_label.items()}
+            
+        labels = LabelClass.objects.get(name=task).labels.all()
+            
+        for _, row in pred_df.iterrows():
+            # check if study already exists
+            id = row['id']
+            # get or create study
+            try:
+                study = Study.objects.get(id=id)
+            except Study.DoesNotExist:
+                Study.objects.create(
+                    id=id,
+                    text=row['text']
+                )
+            probabilities = literal_eval(row['probability'])
+            for i, prob in enumerate(probabilities):
+                label = labels.get(name=int_to_label[i])
+                pred = Prediction.objects.create(
+                    label=label,
+                    model_name=model,
+                    probability=prob
+                )
+                study.predictions.add(pred)
+            study.save()
---
+++ app/psynamic/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ClassGroup(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class LabelClass(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    class_group = models.ForeignKey(
+        ClassGroup, on_delete=models.CASCADE, related_name='classes')
+    is_multilable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('name', 'class_group')
+
+
+class Label(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    label_class = models.ForeignKey(
+        LabelClass, on_delete=models.CASCADE, related_name='labels')
+
+    def __str__(self):
+        return f'{self.name} ({self.label_class.name})'
+
+    class Meta:
+        unique_together = ('name', 'label_class')
+
+
+class Prediction(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=200)
+    probability = models.FloatField()
+
+
+class Study(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(default='') # Title + Abstract as presented in prodigy
+    predictions = models.ManyToManyField(Prediction)
+
+    def __str__(self):
+        return self.title
+
+    def get_prediction(self, label_class: str, prob_threshold: float = 0.5) -> list[str]:
+        # check if label_class exists
+        try:
+            label_class = LabelClass.objects.get(name=label_class)
+        except LabelClass.DoesNotExist:
+            raise ValueError(f'LabelClass {label_class} does not exist')
+
+        labels = label_class.labels.all()
+        # get predictions
+        predictions = self.predictions.filter(label__in=labels)
+
+        if label_class.is_multilable:
+            return [p.label.name for p in predictions if p.probability >= prob_threshold]
+        # if single label return the label with the highest probability
+        else:
+            return [max(predictions, key=lambda x: x.probability).label.name]
